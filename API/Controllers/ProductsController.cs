5cc90ca57c3823a364c0ab93797ec66563150168
Tom Horvath <THorvath@hymans.co.uk>
2020-07-01 11:55:40 +0100

    Added repo and Product controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,23 +10,35 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private StoreContext _context;
+        private IProductRepository _productRepo;
 
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository productRepo)
         {
-            _context = context;
+            _productRepo = productRepo;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetProducts() {
-            var products = await _context.Products.ToListAsync();
+            var products = await _productRepo.GetProductsAsync();
             return Ok(products);
         }
 
         [HttpGet("{productId}")]
         public async Task<IActionResult> GetProduct(int productId) {
-            var product = await _context.Products.FindAsync(productId);
+            var product = await _productRepo.GetProductByIdAsync(productId);
             return Ok(product);
+        }
+        
+        [HttpGet("Types")]
+        public async Task<IActionResult> GetProductTypess() {
+            var productTypes = await _productRepo.GetProductTypesAsync();
+            return Ok(productTypes);
+        }
+        
+        [HttpGet("Brands")]
+        public async Task<IActionResult> GetProductBrands() {
+            var productBrands = await _productRepo.GetProductBrandsAsync();
+            return Ok(productBrands);
         }
     }
 }
